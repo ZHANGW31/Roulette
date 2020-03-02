8510183b6e9a89c3adffd799b47bc1c3f6ec3328
Wei Zhang <wei.zhang4@outlook.com>
2020-03-02 14:00:20 -0800

    Added additional implementation requirements.

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -12,6 +12,19 @@ namespace Roulette
 
     class Roulette
     {
+        //To do
+        //Win cases: 
+        //Numbers: the number of the bin
+        //Evens or Odds: even or odd numbers
+        //Reds or Blacks: red or black colored numbers
+        //Lows or Highs: 1-18 (low) or 19-38 (high)
+        //Dozens: row thirds, 1-12, 13-24, 25-36
+        //Columns: first, second, or third columns
+        //street: rows, 1/2/3 or 22/23/24
+        //6 Numbers: double rows , 1/2/3/4/5/6 or 22/23/24/25/26
+        //Split: at the edge of any two contiguous numbers 1/2 , 11/14,  and 35/36
+        //Corner: at the intersection of any four contiuous numbers , 1/2/4/5 , or 23/24/26/27
+
 
     }
 }
