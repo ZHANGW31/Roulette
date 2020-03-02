b7f2d41cf85f2c60fff49b0b1f57e76e5d2c8168
Wei Zhang <wei.zhang4@outlook.com>
2020-03-02 13:52:33 -0800

    Add project files.

---
+++ Roulette/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Roulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Roulette
+    {
+
+    }
+}
