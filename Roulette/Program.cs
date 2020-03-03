6c40ab20a8a6b3dbc24918bc1b6d06bf3fe2f500
Wei Zhang <wei.zhang4@outlook.com>
2020-03-03 14:15:59 -0800

    implemented the evenorodds method.

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -5,25 +5,53 @@ namespace Roulette
     class Program
     {
         static void Main(string[] args)
-        {
-           
-            Console.WriteLine("The number that the ball landed on is" + RandomizeRoulette());
+        {   
+            //In this array, index 36 and 37 represents 0 and 00 respectively.
+            string[] rouletteArray = new string[] { "Red : 1", "Black : 2", "Red : 3", "Black : 4", "Red : 5",
+                                                    "Black : 6", "Red : 7", "Black : 8", "Red : 9", "Black : 10", "Black : 11", "Red : 12",
+                                                    "Black : 13", "Red : 14", "Black : 15", "Red : 16", "Black : 17", "Red : 18", "Red : 19",
+                                                    "Black : 20", "Red : 21", "Black : 22", "Red : 23", "Black : 24", "Red : 25", "Black : 26",
+                                                    "Red : 27", "Black : 28", "Black : 29", "Red : 30", "Black : 31", "Red : 32", "Black : 33",
+                                                    "Red : 34", "Black : 35", "Red : 36", "Green : 0", "Green : 00" };
 
 
+            int resultNum = RandomizeRoulette(rouletteArray) - 1;
+            Console.WriteLine("The bin that the ball landed on is " + rouletteArray[resultNum]);
+            Console.WriteLine("The result num currently is " + resultNum);
+            resultNum += 1;
+            Console.WriteLine("\nThe winning bets are as follows below: ");
+            //Implementation here
+            Console.WriteLine(BetEvenOrOdd(resultNum));
         }
-        public static int RandomizeRoulette()
+        public static int RandomizeRoulette(string[] rouletteArray)
         {
-            string[] rouletteArray = new string[] { "00", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", 
-                                                    "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
-                                                    "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36" };
             Random random = new Random();
-            for (int i = 0; i < rouletteArray.Length -1 ; i++)
-            {
-                int j = random.Next(i, rouletteArray.Length);
+            int resultIndex = random.Next(rouletteArray.Length - 1);
+            int realNumber = resultIndex + 1;
+            Console.WriteLine("Spinning...");
+            Console.WriteLine(realNumber);
+            return realNumber;
+        }
+        public static string BetEvenOrOdd(int resultNum)
+        {
+            string resultString = "";
 
+            if (resultNum > 36)
+            {
+                resultString += "It is neither even or odd.";
+            }
+            resultNum %= 2;
+            if (resultNum == 0)
+            {
+                resultString += "Evens";
+            }
+            else
+            {
+                resultString += "Odds";
             }
 
-            return 1;
+            return resultString;
+
         }
     }
 
@@ -34,8 +62,8 @@ namespace Roulette
         //Numbers: the number of the bin
         //Evens or Odds: even or odd numbers
         //Reds or Blacks: red or black colored numbers
-        //Lows or Highs: 1-18 (low) or 19-38 (high)
-        //Dozens: row thirds, 1-12, 13-24, 25-36
+        //Lows or Highs: 1:18 (low) or 19:38 (high)
+        //Dozens: row thirds, 1:12, 13:24, 25:36
         //Columns: first, second, or third columns
         //street: rows, 1/2/3 or 22/23/24
         //6 Numbers: double rows , 1/2/3/4/5/6 or 22/23/24/25/26
